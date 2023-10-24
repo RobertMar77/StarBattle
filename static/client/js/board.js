35e50bd7b37a02f81a6eeeaa9f763943c46e5567
Spiddubnyy <spiddubnyy@plu.edu>
2023-10-24 00:36:35 -0700

    Implemented layout rendering, star layout handling, and logic to only allow 2 stars per section

--- static/client/js/board.js
+++ static/client/js/board.js
@@ -13,6 +13,32 @@ export class Board {
         this.starImage.onload = () => this.draw();
         this.starImage.src = "img/star_gold.png";
 
+        this.layout = [
+            [1, 1, 1, 1, 1, 1, 1, 1, 2, 2],
+            [1, 1, 1, 1, 1, 1, 5, 1, 2, 2],
+            [3, 4, 4, 4, 1, 5, 5, 5, 2, 2],
+            [3, 3, 3, 3, 6, 7, 7, 5, 2, 2],
+            [3, 3, 3, 6, 6, 6, 7, 7, 2, 2],
+            [3, 3, 8, 6, 6, 6, 7, 7, 2, 2],
+            [3, 3, 8, 8, 8, 7, 7, 7, 9, 2],
+            [3, 3, 3, 3, 3, 3, 7, 7, 9, 2],
+            [3, 10, 10, 10, 10, 10, 10, 10, 9, 9],
+            [10, 10, 10, 10, 10, 10, 10, 10, 10, 9]
+        ];
+
+        this.stars = [
+            [false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false]
+        ];
+
         // Add an event listener to the canvas to be triggered on mouse click.  When a
         // click occurs, it will call the function mouseClickOnBoard.
         //
@@ -26,6 +52,8 @@ export class Board {
 
         // Draw the Board's background and grid
         this.drawBoard(ctx);
+
+        this.drawLayout(ctx);
         // Draw some pieces
         this.drawExamplePieces(ctx);
     }
@@ -66,8 +94,48 @@ export class Board {
         // Draw a thicker edge
         ctx.lineWidth = 4.5;
         ctx.beginPath();
-        ctx.moveTo( 3 * cellSize, cellSize );
-        ctx.lineTo( 3 * cellSize, 4 * cellSize );
+        ctx.moveTo( 0 * cellSize, 0 * cellSize );
+        ctx.lineTo( 10 * cellSize, 0 * cellSize );
+        ctx.lineTo( 10 * cellSize, 10 * cellSize );
+        ctx.lineTo( 0 * cellSize, 10 * cellSize );
+        ctx.lineTo( 0 * cellSize, 0 * cellSize );
+        ctx.stroke();
+        ctx.closePath();
+
+
+    }
+
+    drawLayout(ctx) {
+        const width = this.canvas.width;
+        const height = this.canvas.height;
+        const cellSize = width / 10.0;  // Assume width = height
+        ctx.beginPath();
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                const curr = this.layout[j][i];
+
+                if (i > 0 && curr != this.layout[j][i-1]) { // Draw line to the left
+                    ctx.moveTo( i * cellSize, j * cellSize );
+                    ctx.lineTo( i * cellSize, (j+1) * cellSize );
+                }
+
+                if (i < 9 && curr != this.layout[j][i+1]) { // Draw line to the right
+                    ctx.moveTo( (i+1) * cellSize, j * cellSize );
+                    ctx.lineTo( (i+1) * cellSize, (j+1) * cellSize );
+                }
+
+                if (j > 0 && curr != this.layout[j-1][i]) { // Draw line above
+                    ctx.moveTo( i * cellSize, j * cellSize );
+                    ctx.lineTo( (i+1) * cellSize, j * cellSize );
+                }
+
+                if (j < 9 && curr != this.layout[j+1][i]) { // Draw line below
+                    ctx.moveTo( i * cellSize, (j+1) * cellSize );
+                    ctx.lineTo( (i+1) * cellSize, (j+1) * cellSize );
+                }
+            }
+        }
+        ctx.closePath();
         ctx.stroke();
     }
 
@@ -87,6 +155,9 @@ export class Board {
      * @param {*} y the y cell number (zero based)
      */
     drawPiece(ctx, x, y) {
+        if (!this.canDrawStar(x, y)) {
+            return;
+        }
         const width = this.canvas.width;
         const height = this.canvas.height;
         const cellSize = width / 10.0;  // Assume width = height
@@ -95,6 +166,27 @@ export class Board {
         const cy = y * cellSize;
 
         ctx.drawImage(this.starImage, cx, cy);
+
+        this.stars[y][x] = true;
+    }
+
+    canDrawStar(x, y) {
+        const curr = this.layout[y][x];
+        let numStarsInSection = 0;
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                if (this.layout[j][i] == curr) {
+                    if (this.stars[j][i] == true) {
+                        numStarsInSection++;
+                        if (numStarsInSection > 1) {
+                            console.log("Too many stars!");
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     /**
@@ -120,6 +212,8 @@ export class Board {
         const cellY = Math.floor( mouseY / cellSize );
 
         console.log(`Click on cell: (${cellX}, ${cellY})`);
+
+        this.drawPiece(this.canvas.getContext('2d'), cellX, cellY);
     }
 
 }
