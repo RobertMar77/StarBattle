c86c13b79a30ae5b6251d4de30a4fab941922d6a
Emmanuel <obikwelu@plu.edu>
2023-11-05 19:55:33 -0800

    fix cell

--- src/main/java/starb/server/transfer/Cell.java
+++ src/main/java/starb/server/transfer/Cell.java
@@ -1,14 +1,15 @@
 package starb.server.transfer;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Cell {
     private int row;
     private int col;
 
-    public Cell(){
 
-    }
-
-    public Cell(int row, int col) {
+    @JsonCreator
+    public Cell(@JsonProperty("row") int row, @JsonProperty("col") int col) {
         this.row = row;
         this.col = col;
     }
